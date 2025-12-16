ea71765ea4c0ecf6a9ce1f12559d9422822bc1d6
NguyenDucTai <nguyenductaicity123@gmail.com>
2025-12-16 14:10:27 +0700

    add presonnal info to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,6 +15,9 @@ function App() {
       <p>
         Backend: {import.meta.env.VITE_API_URL}
       </p>
+      <p><b>Ho Ten:</b>Nguyen Duc Tai</p>
+      <p><b>MSSV:</b>DH52201386</p>
+      <p><b>Lop:</b>D22_TH09</p>
       <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
